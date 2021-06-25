adb9ebf49440e4249ad9549f2dead166712f376a
alrtn4 <45690266+alrtn4@users.noreply.github.com>
2021-06-25 20:04:26 +0430

    Add project files.

---
+++ AutoGallery/Repositories/BannerImageRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoGallery.Models;
+
+namespace AutoGallery.Repositories
+{
+    public class BannerImageRepository : IRepository
+    {
+        private readonly MyDbContext _context;
+
+        //public BannerImageRepository()
+        //{
+
+        //}
+        public BannerImageRepository(MyDbContext context)
+        {
+            _context = context;
+        }
+        public BannerImage GetBannerImage(int id)
+        {
+            return _context.BannerImages.FirstOrDefault(a => a.Id == id);
+        }
+
+        public void AddBannerImage(BannerImage bannerImage)
+        {
+            _context.BannerImages.Add(bannerImage);
+
+            _context.SaveChanges();
+        }
+
+    }
+}
